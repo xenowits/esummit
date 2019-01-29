4303d662aba2b4e18e249da0debb577847873b6e
abhis324 <abhishekmaphy98@gmail.com>
2019-01-29 22:02:44 +0530

    registration work done database and table fields need to be incorporated accordingly

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 const express = require('express')
 const app = express()
 
+const mysql = require('mysql') 
+
 const path = require('path')
 
 const helmet = require('helmet')
+app.use(helmet())
+
+const ejs = require('ejs')
+app.set('view engine', 'ejs')
 
 // ejsLint(, options);
 
@@ -14,7 +20,32 @@ var server = app.listen(port, function(){
   console.log('listening on :' + port);
 });
 
-app.use(helmet())
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+// Actual fields to be added according to the form
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  var sqlDb = "CREATE DATABASE db"
+  var sqlTable = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255))";
+
+  con.query(sqlDb, function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+
+  con.query(sqlTable, (err,result) => {
+  	if (err) throw err;
+  	console.log("Table created")
+  })
+
+});
 
 app.get('/rulebook', (req,res) => {
 
@@ -24,7 +55,19 @@ app.get('/rulebook', (req,res) => {
 
 app.post('/register', (req,res) => {
 
-	res.send("you are successfully registered")
+	con.connect(function(err) {
 
+  	if (err) throw err;
+  	console.log("Connected!");
+
+  	var sql = "INSERT INTO customers (name, email) VALUES (req.body.name , req.body.email )";
+
+  	con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
+
+	res.render('goTohomePage')
 
 })
